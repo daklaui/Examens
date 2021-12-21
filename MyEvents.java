bc90c51bd0df8dfae17635e407259eeb7599ca4c
mootez dakhlaoui <mootez.dakhlaoui@proxym-it.com>
2021-12-21 20:04:06 +0100

    add action listener

--- MyEvents.java
+++ MyEvents.java
@@ -2,8 +2,19 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class MyEvents implements ActionListener {
+    MyForm form ;
+
+    public MyEvents(MyForm form) {
+        this.form = form;
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
+        //Si l'action sur le boutton "Valider"
+        if(e.getSource()==this.form.ajouter)
+        {
+
+        }
 
     }
 }
